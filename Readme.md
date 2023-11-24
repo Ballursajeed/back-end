a1a775b701f217b6ff2cd5f4e336e79cdba82f8a
Ballursajeed <ballursajeed@gmail.com>
2023-11-24 20:46:50 +0530

    start add initial files

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# back-end learning
+
+this is a demo for my back-end,
+i am going to build a major project using node.js and mongoDB in the future
--- index.js
+++ index.js
@@ -1,26 +1,11 @@
-console.log("back end day 1");
-
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World for node.js');
+});
 
-app.get('/twitter',(req,res)=>{
- res.send("ballursajeed")
-})
-
-app.get('/login',(req,res)=>{
- res.send("<h1>Please login at chai wur code</h1>")
-})
-
-app.get("/youtube",(req,res)=>{
- res.send("<h2>chai aur code</h2>")
-})
-
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server listening at http:localhost:${port}`);
+});

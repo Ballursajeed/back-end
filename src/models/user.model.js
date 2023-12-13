d20fbe849f7250cb38908240df92615ede34ad59
Ballursajeed <ballursajeed@gmail.com>
2023-12-14 05:18:24 +0530

    updated some files

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -58,11 +58,11 @@ const userSchema = new Schema(
    next()
  })
 
-useSchema.methods.isPasswordCorrect = async function (password) {
+userSchema.methods.isPasswordCorrect = async function (password) {
  return await bcrypt.compare(password, this.password)
 }
 
-useSchema.methods.generateAccessToken = function(){
+userSchema.methods.generateAccessToken = function(){
    return jwt.sign(
         {
            _id: this._id,
@@ -76,7 +76,7 @@ useSchema.methods.generateAccessToken = function(){
         }
     )
 }
-useSchema.methods.generateRefreshToken = function(){
+userSchema.methods.generateRefreshToken = function(){
        return jwt.sign(
         {
            _id: this._id,

de0f5b47c74657095d903c3673b85f43922f1cae
Ballursajeed <ballursajeed@gmail.com>
2023-12-14 05:01:45 +0530

    added logic for register route

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,27 @@
 import { asyncHandler } from "../utils/asynHandler.js";
+import { ApiError } from "../utils/ApiError.js"
 
 const registerUser = asyncHandler( async (req,res) => {
-   res.status(200).json({ message: "from post" })
+   //get data from frontend
+   //validation - not empty
+   //check if user already exists: username,email
+   //check for image, check for avatar
+   //upload them to cloudinary, avatar
+   //create user object - create entry in db
+   //remove password and refresh token field from response
+   //check for user creation
+   //return response
+
+   const { fullname, email, username, password } = req.body
+   console.log("Email:",email);
+
+  if (
+    [fullname, email, username, password].some((field) =>
+    field?.trim() === "" )
+  ){
+     throw new ApiError(400,"All fields are required")
+  }
+
 } )
 
 export { registerUser, }

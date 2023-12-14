e93ec0b2b31fe557f164c0946f4435e4058e475b
Ballursajeed <ballursajeed@gmail.com>
2023-12-15 02:29:49 +0530

    fix bugs in the register controll

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -37,10 +37,15 @@ const registerUser = asyncHandler( async (req,res) => {
        throw new ApiError(409, "User with email or username already exist")
        console.log("The full name:",fullName);
    }
-
+   console.log(req.files);
 //4) check image and check avatar
   const avatarLocalPath =  req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  let coverImagePath;
+  if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length >0 ) {
+        coverImagePath = req.files.coverImage[0].path;
+  }
 
  if (!avatarLocalPath) {
      throw  new ApiError(400, "Avatar file is required");
@@ -49,7 +54,7 @@ const registerUser = asyncHandler( async (req,res) => {
 
 //5) upload them cloudinary
   const avatar = avatarLocalPath; // await uploadOnCloudinary(avatarLocalPath);
-  const coverImage = coverImageLocalPath //await uploadOnCloudinary(coverImageLocalPath);
+  const coverImage = coverImagePath //await uploadOnCloudinary(coverImageLocalPath);
 
  if (!avatar) {
        throw  new ApiError(400, "Avatar file is required");

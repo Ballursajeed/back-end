8c23be18891b18ff43d75a2c6678b49166f7bfc7
Ballursajeed <ballursajeed@gmail.com>
2023-12-14 12:05:44 +0530

    added controllers

--- src/utils/asynHandler.js
+++ src/utils/asynHandler.js
@@ -1,11 +1,11 @@
 const asyncHandler = (requestHandler) =>{
-   (req,res,next) =>{
+   return (req,res,next) =>{
      Promise.resolve(requesrHandler(req,res,next))
      .catch((err)=> next(err))
    }
 }
 
-export { asyncHandler }
+export { asyncHandler, }
 
 
 
